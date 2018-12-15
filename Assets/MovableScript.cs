c335cee16fe84b8fb623aa26f8147dc1b16b290b
Kyle Jones <kj16wn@brocku.ca>
2018-12-15 00:52:57 +0000

    started working on highlighting in movablescript

--- Assets/MovableScript.cs
+++ Assets/MovableScript.cs
@@ -2,41 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 public class MovableScript : MonoBehaviour {
-    
-	void Start () {
-		
-	}
-    GameObject heldObject;
-
+    GameObject heldPiece;
+    Vector2 heldPiecePrevPos;
+    public GameObject[] board;
 	void Update () {
 
-		if (!heldObject && Input.GetAxis("Fire1") != 0)
+		if (!heldPiece && Input.GetAxis("Fire1") != 0)
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "chessPiece")
-            {
-                heldObject = hit.transform.gameObject;
-                
-            }
+            TryPickup();
         }
-        if (heldObject)
+        else if (heldPiece)
         {
-            Vector2 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            heldObject.transform.position = new Vector3(newPos.x, newPos.y, heldObject.transform.position.z);
-
-            if (Input.GetAxis("Fire1") == 0)
-            {
-                heldObject.transform.position = new Vector3(
-                    Mathf.RoundToInt(heldObject.transform.position.x),
-                    Mathf.RoundToInt(heldObject.transform.position.y),
-                    heldObject.transform.position.z
-                );
-                heldObject = null;
-            }
-
+            MoveToCursor();
         }
 	}
+
+    void TryPickup()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit) && hit.transform.tag == "chessPiece")
+        {
+            heldPiece = hit.transform.gameObject;
+            heldPiecePrevPos = heldPiece.transform.position;
+            HighlightValidMoves();
+        }
+    }
+
+    void MoveToCursor()
+    {
+        Vector2 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        heldPiece.transform.position = new Vector3(newPos.x, newPos.y, heldPiece.transform.position.z);
+
+        if (Input.GetAxis("Fire1") == 0)
+        {
+            heldPiece.transform.position = new Vector3(
+                Mathf.RoundToInt(heldPiece.transform.position.x),
+                Mathf.RoundToInt(heldPiece.transform.position.y),
+                heldPiece.transform.position.z
+            );
+            heldPiece = null;
+        }
+    }
+
+    void HighlightValidMoves()
+    {
+        PieceScript pieceScript = heldPiece.GetComponent<PieceScript>();
+        switch (pieceScript.pieceType)
+        {
+            case PieceScript.PieceType.Pawn:
+                int initPos = (int) ((heldPiecePrevPos.y - 1) * 8 + (heldPiecePrevPos.x - 1));
+                HighlightPos(initPos + 8);
+                if ()
+                {
+
+                }
+                break;
+            case PieceScript.PieceType.Rook:
+                break;
+            case PieceScript.PieceType.Bishop:
+                break;
+            case PieceScript.PieceType.Knight:
+                break;
+            case PieceScript.PieceType.Queen:
+                break;
+            case PieceScript.PieceType.King:
+                break;
+        }
+    }
+    void HighlightPos(int index)
+    {
+        if (index >= 0 && index < board.Length)
+        {
+            //highlight
+        }
+    }
 }
---
+++ Assets/PieceScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceScript : MonoBehaviour {
+
+    public enum PieceType
+    {
+        Pawn,
+        Rook,
+        Bishop,
+        Knight,
+        Queen,
+        King
+    }
+    public Vector3 homePosition;
+
+
+    public PieceType pieceType;
+
+    private void Start()
+    {
+        homePosition = transform.position;
+    }
+}
