3ca27b3f4a91a9532b5ddd28b1e187917bbeb021
Robbie Jolley <robbie.halladay.34@gmail.com>
2018-12-18 22:38:11 +0000

    Knight movement done

--- Assets/MovableScript.cs
+++ Assets/MovableScript.cs
@@ -141,6 +141,127 @@ public class MovableScript : MonoBehaviour {
                 }
                 break;
             case PieceScript.PieceType.Knight:
+                initPos = (int)((heldPiece.LastValidPosition.y - 1) * 8 + (heldPiece.LastValidPosition.x - 1));
+                int rowsThrough = 0;
+
+                for (int i=0; i<6; i++)
+                {
+                    if (i == 0)
+                        rowsThrough = 0;
+                    if ((initPos-i) % 8 == 0)
+                    {
+                        rowsThrough++;
+                    }
+                    if (i == 5 && rowsThrough == 1)
+                    {
+                        if (initPos % 8 == 0)
+                            rowsThrough++;
+                        MarkMove(initPos - 6);
+                    }
+                }
+                for (int i = 0; i < 6; i++)
+                {
+                    if (i == 0)
+                        rowsThrough = 0;
+                    if ((initPos + i+1) % 8 == 0)
+                    {
+                        rowsThrough++;
+                    }
+                    if (i == 5 && rowsThrough == 1)
+                    {
+                        MarkMove(initPos + 6);
+                    }
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == 0)
+                        rowsThrough = 0;
+                    if ((initPos +i) % 8 == 0)
+                    {
+                        rowsThrough++;
+                    }
+                    if (i == 9 && (initPos + 2) % 8 == 0)
+                        rowsThrough = rowsThrough + 6;
+                    else if (i == 9 && initPos % 8 == 0)
+                        rowsThrough = 1;
+                   if (i == 9 && rowsThrough == 1)
+                    {
+                        MarkMove(initPos + 10);
+                    }
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == 0)
+                        rowsThrough = 0;
+                    if ((initPos - i) % 8 == 0)
+                    {
+                        rowsThrough++;
+                    }
+                    if (i == 9 && (initPos + 1) % 8 == 0)
+                        rowsThrough = 1;
+                    if (i == 9 && rowsThrough == 1)
+                    {
+                        MarkMove(initPos - 10);
+                    }
+                }
+                for (int i = 0; i < 15; i++)
+                {
+                    if (i == 0)
+                        rowsThrough = 0;
+                    if ((initPos + i) % 8 == 0)
+                    {
+                        rowsThrough++;
+                    }
+                    if (i == 14 && initPos % 8 == 0)
+                        rowsThrough = 1;
+                    if (i == 14 && rowsThrough == 2)
+                    {
+                        MarkMove(initPos + 15);
+                    }
+                }
+                for (int i = 0; i < 15; i++)
+                {
+                    if (i == 0)
+                        rowsThrough = 0;
+                    if ((initPos - i) % 8 == 0)
+                    {
+                        rowsThrough++;
+                    }
+                    if (i == 14 && rowsThrough == 2)
+                    {
+                        MarkMove(initPos - 15);
+                    }
+                }
+                for (int i = 0; i < 17; i++)
+                {
+                    if (i == 0)
+                        rowsThrough = 0;
+                    if ((initPos + i) % 8 == 0)
+                    {
+                        rowsThrough++;
+                    }
+                    if (i == 16 && (initPos + 1) % 8 == 0)
+                        rowsThrough = rowsThrough + 6;
+                    else if (i == 16 && initPos % 8 == 0)
+                        rowsThrough = 2;
+                    if (i == 16 && rowsThrough == 2)
+                    {
+                        MarkMove(initPos + 17);
+                    }
+                }
+                for (int i = 0; i < 17; i++)
+                {
+                    if (i == 0)
+                        rowsThrough = 0;
+                    if ((initPos - i) % 8 == 0)
+                    {
+                        rowsThrough++;
+                    }
+                    if (i == 16 && rowsThrough == 2)
+                    {
+                        MarkMove(initPos - 17);
+                    }
+                }
                 break;
             case PieceScript.PieceType.Queen:
                 break;
